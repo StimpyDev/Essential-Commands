fc2c542b2823147218d09b264a256464a1c5e4ed
Stimpy <95110502+StimpyDev@users.noreply.github.com>
2026-04-24 16:49:06 +0200

    Update TeleportManager.java

--- src/main/java/com/fibermc/essentialcommands/teleportation/TeleportManager.java
+++ src/main/java/com/fibermc/essentialcommands/teleportation/TeleportManager.java
@@ -30,8 +30,8 @@ public final class TeleportManager {
 
     private TeleportManager() {
         instance = this;
-        activeTeleportRequests = new LinkedList<>();
-        playersOnTeleportCooldown = new LinkedList<>();
+        activeTeleportRequests = new ArrayList<>();
+        playersOnTeleportCooldown = new ArrayList<>();
         queuedTeleportMap = new ConcurrentHashMap<>();
     }
 
@@ -49,72 +49,83 @@ public final class TeleportManager {
     }
 
     public void tick(MinecraftServer server) {
-        activeTeleportRequests.removeIf(TeleportRequest::isEnded);
-        
-        for (TeleportRequest teleportRequest : activeTeleportRequests) {
-            teleportRequest.incrementAgeTicks();
-
-            if (teleportRequest.getAgeTicks() > CONFIG.TELEPORT_REQUEST_DURATION_TICKS) {
-                teleportRequest.end();
-                teleportRequest.getSenderPlayerData().sendMessage(
-                    "teleport.request.expired.sender",
-                    teleportRequest.getTargetPlayer().getDisplayName()
-                );
-                teleportRequest.getTargetPlayerData().sendMessage(
-                    "teleport.request.expired.receiver",
-                    teleportRequest.getSenderPlayer().getDisplayName()
-                );
-            }
+        if (activeTeleportRequests.size() == 0 && queuedTeleportMap.size() == 0 && playersOnTeleportCooldown.size() == 0) {
+            return;
         }
 
-        ListIterator<PlayerData> tpCooldownIterator = playersOnTeleportCooldown.listIterator();
-        while (tpCooldownIterator.hasNext()) {
-            PlayerData playerData = tpCooldownIterator.next();
-            playerData.tickTpCooldown();
-            if (playerData.getTpCooldown() < 0) {
-                tpCooldownIterator.remove();
-            }
-        }
+        if (activeTeleportRequests.size() > 0) {
+            Iterator<TeleportRequest> requestIter = activeTeleportRequests.iterator();
+            while (requestIter.hasNext()) {
+                TeleportRequest request = requestIter.next();
+                request.incrementAgeTicks();
 
-        var shouldInterruptTeleportOnMove = CONFIG.TELEPORT_INTERRUPT_ON_MOVE;
-        var maxMoveBeforeInterrupt = CONFIG.TELEPORT_INTERRUPT_ON_MOVE_AMOUNT;
-        Iterator<Map.Entry<UUID, QueuedTeleport>> tpQueueIter = queuedTeleportMap.entrySet().iterator();
-        
-        while (tpQueueIter.hasNext()) {
-            Map.Entry<UUID, QueuedTeleport> entry = tpQueueIter.next();
-            QueuedTeleport queuedTeleport = entry.getValue();
-            queuedTeleport.tick(server);
-
-            var playerData = queuedTeleport.getPlayerData();
-            var player = playerData.getPlayer();
-            var playerAccess = (ServerPlayerEntityAccess) player;
-            
-            long ticksRemaining = queuedTeleport.getTicksRemaining();
-            
-            if (ticksRemaining > 0 && ticksRemaining % 20 == 0) {
-                int secondsRemaining = (int) (ticksRemaining / 20);
-                var accentStyle = playerAccess.ec$getProfile().getStyle(TextFormatType.Accent);
+                if (request.getAgeTicks() > CONFIG.TELEPORT_REQUEST_DURATION_TICKS) {
+                    request.end();
+                    request.getSenderPlayerData().sendMessage("teleport.request.expired.sender", request.getTargetPlayer().getDisplayName());
+                    request.getTargetPlayerData().sendMessage("teleport.request.expired.receiver", request.getSenderPlayer().getDisplayName());
+                    requestIter.remove();
+                    continue;
+                }
                 
-                playerData.sendMessage(
-                    "teleport.queued",
-                    queuedTeleport.getDestName(),
-                    Component.literal(String.valueOf(secondsRemaining)).withStyle(accentStyle)
-                );
+                if (request.isEnded()) {
+                    requestIter.remove();
+                }
             }
+        }
 
-            if (shouldInterruptTeleportOnMove
-                && playerData.hasMovedThisTick()
-                && player.position().distanceTo(queuedTeleport.initialPosition) > maxMoveBeforeInterrupt
-                && !PlayerTeleporter.playerHasTpRulesBypass(player, ECPerms.Registry.bypass_teleport_interrupt_on_move)
-            ) {
-                playerData.sendError("teleport.interrupted.moved");
-                tpQueueIter.remove();
-                continue;
+        if (playersOnTeleportCooldown.size() > 0) {
+            ListIterator<PlayerData> tpCooldownIterator = playersOnTeleportCooldown.listIterator();
+            while (tpCooldownIterator.hasNext()) {
+                PlayerData playerData = tpCooldownIterator.next();
+                playerData.tickTpCooldown();
+                if (playerData.getTpCooldown() < 0) {
+                    tpCooldownIterator.remove();
+                }
             }
+        }
 
-            if (ticksRemaining < 0) {
-                tpQueueIter.remove();
-                PlayerTeleporter.teleport(queuedTeleport);
+        if (queuedTeleportMap.size() > 0) {
+            var shouldInterruptTeleportOnMove = CONFIG.TELEPORT_INTERRUPT_ON_MOVE;
+            var maxMoveBeforeInterrupt = CONFIG.TELEPORT_INTERRUPT_ON_MOVE_AMOUNT;
+            Iterator<Map.Entry<UUID, QueuedTeleport>> tpQueueIter = queuedTeleportMap.entrySet().iterator();
+            
+            while (tpQueueIter.hasNext()) {
+                Map.Entry<UUID, QueuedTeleport> entry = tpQueueIter.next();
+                QueuedTeleport queuedTeleport = entry.getValue();
+                queuedTeleport.tick(server);
+
+                var playerData = queuedTeleport.getPlayerData();
+                var player = playerData.getPlayer();
+                var playerAccess = (ServerPlayerEntityAccess) player;
+                
+                long ticksRemaining = queuedTeleport.getTicksRemaining();
+                
+                if (ticksRemaining > 0 && ticksRemaining % 20 == 0) {
+                    int secondsRemaining = (int) (ticksRemaining / 20);
+                    var accentStyle = playerAccess.ec$getProfile().getStyle(TextFormatType.Accent);
+                    
+                    playerData.sendMessage(
+                        "teleport.queued",
+                        queuedTeleport.getDestName(),
+                        Component.literal(String.valueOf(secondsRemaining)).withStyle(accentStyle)
+                    );
+                }
+
+                if (shouldInterruptTeleportOnMove
+                    && playerData.hasMovedThisTick()
+                    && player.position().distanceTo(queuedTeleport.initialPosition) > maxMoveBeforeInterrupt
+                    && !PlayerTeleporter.playerHasTpRulesBypass(player, ECPerms.Registry.bypass_teleport_interrupt_on_move)
+                ) {
+                    playerData.sendError("teleport.interrupted.moved");
+                    tpQueueIter.remove();
+                    continue;
+                }
+
+                // Voer teleportatie uit
+                if (ticksRemaining < 0) {
+                    tpQueueIter.remove();
+                    PlayerTeleporter.teleport(queuedTeleport);
+                }
             }
         }
     }
@@ -166,9 +177,10 @@ public final class TeleportManager {
         var playerAccess = ((ServerPlayerEntityAccess) playerData.getPlayer());
 
         QueuedTeleport prevValue = queuedTeleportMap.put(
-            queuedTeleport.getPlayerData().getPlayer().getUUID(),
+            playerData.getPlayer().getUUID(),
             queuedTeleport
         );
+        
         if (prevValue != null) {
             var profile = playerAccess.ec$getProfile();
             var styleUpdater = profile.nonOverwritingColorUpdater(TextFormatType.Accent);
