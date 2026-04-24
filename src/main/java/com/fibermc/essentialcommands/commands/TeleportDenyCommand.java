c0ea5778a8c035db1d98c86ad80a956e5d617338
Stimpy <95110502+StimpyDev@users.noreply.github.com>
2026-04-24 16:52:11 +0200

    Update TeleportDenyCommand.java

--- src/main/java/com/fibermc/essentialcommands/commands/TeleportDenyCommand.java
+++ src/main/java/com/fibermc/essentialcommands/commands/TeleportDenyCommand.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import com.fibermc.essentialcommands.playerdata.PlayerData;
 import com.fibermc.essentialcommands.teleportation.TeleportRequest;
 
+import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
 
 import net.minecraft.commands.CommandSourceStack;
@@ -14,13 +15,14 @@ public class TeleportDenyCommand extends TeleportResponseCommand {
     
     @Override
     protected int exec(CommandContext<CommandSourceStack> context, ServerPlayer respondingPlayer, ServerPlayer requesterPlayer) {
-        var requesterPlayerData = PlayerData.access(requesterPlayer);
         var respondingPlayerData = PlayerData.access(respondingPlayer);
 
         if (requesterPlayer == null) {
             respondingPlayerData.sendError("cmd.tpa_reply.error.no_request_from_target");
             return -1;
         }
+
+        var requesterPlayerData = PlayerData.access(requesterPlayer);
 
         Optional<TeleportRequest> teleportRequest = requesterPlayerData.getSentTeleportRequests()
             .getRequestToPlayer(respondingPlayerData);
@@ -34,14 +36,14 @@ public class TeleportDenyCommand extends TeleportResponseCommand {
             "cmd.tpdeny.feedback",
             respondingPlayer.getDisplayName()
         );
-
-        respondingPlayerData.sendCommandFeedback(
+        
+        respondingPlayerData.sendMessage(
             "cmd.tpdeny.feedback",
             requesterPlayer.getDisplayName()
         );
 
         teleportRequest.get().end();
-
-        return SINGLE_SUCCESS;
+        
+        return Command.SINGLE_SUCCESS;
     }
 }
