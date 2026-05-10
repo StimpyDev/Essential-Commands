c6012d967e0d3823eebb543a7000e005c0a23778
Stimpy <95110502+StimpyDev@users.noreply.github.com>
2026-05-10 13:41:44 +0200

    Update TeleportAskHereCommand.java

--- src/main/java/com/fibermc/essentialcommands/commands/TeleportAskHereCommand.java
+++ src/main/java/com/fibermc/essentialcommands/commands/TeleportAskHereCommand.java
@@ -6,7 +6,7 @@ import com.fibermc.essentialcommands.teleportation.TeleportManager;
 import com.fibermc.essentialcommands.teleportation.TeleportRequest;
 import com.fibermc.essentialcommands.text.ChatConfirmationPrompt;
 import com.fibermc.essentialcommands.text.ECText;
-
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
